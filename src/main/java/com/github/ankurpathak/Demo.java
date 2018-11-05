c88d5d9f076aa68446b545948f7c2731c0a296e9
Ankur Pathak <ankurpathak@live.in>
2018-11-05 19:15:39 +0530

    demo corrected

--- src/main/java/com/github/ankurpathak/Demo.java
+++ src/main/java/com/github/ankurpathak/Demo.java
@@ -12,26 +12,25 @@ public class Demo {
     public static void main(String[] args) throws Exception {
 
 
-        System.out.println(JAXBContext.newInstance(MapObject.class).getClass());
-
-        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
-
-        File xml = new File(classLoader.getResource("input.xml").getFile());
-
-        System.out.println("File Found : " + xml.exists());
         JAXBContext jc = JAXBContext.newInstance(MapObject.class);
-
-        Unmarshaller unmarshaller = jc.createUnmarshaller();
-        //MapObject root = (MapObject) unmarshaller.unmarshal(xml);
+        System.out.println(jc.getClass());
 
         MapObject root = new MapObject();
         root.setMap(Map.of("a", 1, "b", 2));
 
         Marshaller marshaller = jc.createMarshaller();
         marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-       // marshaller.setProperty(MarshallerProperties.MEDIA_TYPE, "application/xml");
-       // marshaller.setProperty(MarshallerProperties.JSON_INCLUDE_ROOT, false);
+        marshaller.setProperty(MarshallerProperties.MEDIA_TYPE, "application/xml");
         marshaller.marshal(root, System.out);
     }
 
 }
+
+/*
+
+ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+
+        File xml = new File(classLoader.getResource("input.xml").getFile());
+
+        System.out.println("File Found : " + xml.exists());
+ */
